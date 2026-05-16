b6ec504ae5a3d9dbfa74da85861981fe9b2b932c
Alejandro Alisajar <aalisajar@estudiante.uniajc.edu.co>
2026-05-16 00:03:08 -0500

    feat: agregar calcular el perimetro y el diametro de un circulo mediante su rradio

--- funcion.java
+++ funcion.java
@@ -4,9 +4,21 @@ public class funcion {
         return Math.PI * Math.pow(radio, 2);
     }
 
+    public static double calcularPerimetro(double radio){
+        return 2*Math.PI*radio;  
+    }
+
+    public static double calcularDiametro(double radio){
+        return 2*radio;
+    }
+    
     public static void main(String[] args) {
         double radio = 5.0;
         double area = calcularArea(radio);
+        double perimetro = calcularPerimetro(radio);
+        double diametro = calcularDiametro(radio);
         System.out.println("El área del círculo con radio " + radio + " es: " + area);
+        System.out.println("El perímetro del círculo con radio " + radio + " es: " + perimetro);
+        System.out.println("El diámetro del círculo con radio " + radio + " es: " + diametro);
     }
 }
